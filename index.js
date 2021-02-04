e6b66b1f6c81cea36e6816be1836fd956de27004
ccdmndkut <ccdmndkut@gmail.com>
2021-02-04 01:31:51 -0500

    d

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello Borld!'))
-
+app.get('/b', (req, res) => res.send('Hello Borld!'))
 app.listen(port, () =>
   console.log(`sample-expressjs app listening on port ${port}!`)
 )
